d1936f61ec47a227a21ecd8a6a8e658b9dc29ba5
MRafiramadhanK <raficool271003@gmail.com>
2024-06-03 10:33:47 +0700

    melemparkan sebuah interger

---
+++ manualException/manualException.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<exception>
+using namespace std;
+
+int main() {
+	try {
+		cout << "selamat belajar di prodi TI UMY" << endl;
+		cout << "pernyataan tidak dieksekusi" << endl;
+	}
+	catch (int a) {
+
+	}
+}
